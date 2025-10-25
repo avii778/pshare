6b37ac0ee56ca33d939bb0984ce39bb5b23b4558
avii778 <142438557+avii778@users.noreply.github.com>
2025-10-25 18:12:31 -0400

    Made reading/writing non blocking, added basic cli functionality

--- p2pcli/src/main.rs
+++ p2pcli/src/main.rs
@@ -3,8 +3,10 @@ mod networks;
 
 use cli::{CLI, Cmd};
 use clap::Parser;
+use crate::networks::Connection;
 
-fn main() {
+#[tokio::main]
+async fn main() {
 
     let args = CLI::parse();
 
@@ -12,10 +14,45 @@ fn main() {
 
         Cmd::Receive {bind, out_dir} => {
 
+            let networks = Connection::new_connection(bind).await;
+
+            match networks.unwrap() {
+                Err(E) => {println!("Error communicating with address")}
+
+                Ok(connection) => {
+                    let read = connection.read_file(out_dir).await;
+
+                    match read.unwrap() {
+
+                        Err(E) => {println!("Error reading from stream")}
+
+                        Ok(T) => {println!("Recieved file, stored at: {}", out_dir.as_os_str())}
+                    }
+                }
+            }
+
         }
 
         Cmd::Send {bind, file} => {
 
+            let networks = Connection::new_connection(bind).await;
+
+            match networks.unwrap() {
+                Err(E) => {println!("Error communicating with address")}
+
+                Ok(connection) => {
+
+                    let write = connection.write_file(file);
+
+                    match write.unwrap() {
+
+                        Err(E) => {println!("Error writing to stream")}
+
+                        Ok(T) => {println!("Recieved file, stored at: {}", file.as_os_str())}
+                    }
+
+                }
+            }
         }
 
     }
--- p2pcli/src/networks.rs
+++ p2pcli/src/networks.rs
@@ -1,12 +1,11 @@
 use std::alloc::System;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::fs;
+use tokio::fs::OpenOptions;
 
 use std::io::{self, Read, Write};
 use std::fs::File;
 use std::path::Path;
-use std::fs::OpenOptions;
 
 pub struct Connection {
     bind: String, // should be some ip
@@ -15,36 +14,47 @@ pub struct Connection {
 
 impl Connection {
 
-    pub async fn network (&mut self, bind: String) -> io::Result<()>{
-        self.bind = bind;
-        self.listener = TcpListener::bind(&self.bind).await?;
+    pub async fn new_connection(bind: String) -> Result<Connection, io::Error>{
 
+        let listener = TcpListener::bind(&bind).await?;
+
+        // If bind succeeded, return Ok(Connection)
+        Ok(Connection {
+            bind,
+            listener,
+        })
+
+    }
+    pub async fn send_file<P: AsRef<Path>>(&self, file: P) -> io::Result<()>{
+        let (mut socket, _) = self.listener.accept().await?;
+
+        let mut file = OpenOptions::new()
+            .read(true)
+            .create(true)// send an empty file if needed
+            .truncate(false)  // DO NOT WIPE CONTENTS. DO NOT CHANGE
+            .open(file)
+            .await?;
+
+
+        tokio::io::copy(&mut file, &mut socket).await?;
         Ok(())
     }
-    pub async fn send_file(&self, file: String) -> io::Result<()>{
-        let contents = tokio::fs::read_to_string(file).await?;
+
+    pub async fn read_file<P: AsRef<Path>>(&self, path_to_write: P) -> io::Result<()>{
 
         let (mut socket, _) = self.listener.accept().await?;
 
-        socket.write_all(contents.as_ref()).await?;
-        Ok(())
-    }
+        let mut file =
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(path_to_write)
+                .await?;
 
-    pub async fn read_file(&self, path_to_write: String) -> io::Result<()>{
+        tokio::io::copy(&mut socket, &mut file).await?;
 
-        let (mut socket, _) = self.listener.accept().await?;
-
-        let path = Path::new(&path_to_write);
-
-        let mut file = OpenOptions::new().write(true).open(path_to_write);
-        let mut buffer = Vec::new();
-        socket.read(&mut buffer).await.expect("Error reading from stream ");
-
-        match file?.write(&*buffer).await {
-
-            Err(e) => {println!("Error writing to file")}
-        }
-
+        file.flush().await?;
 
         Ok(())
 
